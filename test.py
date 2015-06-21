1882894fddff342f042da15b7953b4c74e1a3828
John <japarker@uchicago.edu>
2015-06-21 15:33:29 -0500

    added test.py, to eventually plot 2D animations

--- test.py
+++ test.py
@@ -1,21 +1,37 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import pylab as py
+from mpl_toolkits.mplot3d import Axes3D
+from multiprocessing import Pool,Process,Value
 
-fig, ax = plt.subplots()
+pool = Pool()
 
-x = np.arange(0, 2*np.pi, 0.01)        # x-array
-line, = ax.plot(x, np.sin(x))
+x = np.linspace(-5,5,50)
+y = np.linspace(-5,5,50)
+x,y = np.meshgrid(x,y)
+z = x*0
 
-def animate(i):
-    line.set_ydata(np.sin(x+i/10.0))  # update the data
-    return line,
+def f(a,b,c):
+    for i in range(a,b):
+        print(c.value)
+        fig = py.figure(i)
+        ax = fig.gca(projection='3d')
+        R = np.sqrt(x**2+y**2)
+        z = np.sin(R + i/10.0)
+        surf = ax.plot_surface(x,y,z,rstride=1,cstride=1)
+        py.savefig(r'./figs/fig' + str(i+1).zfill(3) + '.png')
+        py.close(i)
+        c.value += 1
 
-#Init only required for blitting to give a clean slate.
-def init():
-    line.set_ydata(np.ma.array(x, mask=True))
-    return line,
+proc = []
+cores = 4
+steps = 100
+size = int(np.floor(steps/cores))
+c = Value('i', 0)
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
-                            interval=25, blit=True)
-plt.show()
+for i in range(cores):
+    p = Process(target=f, args=(i*size, (i+1)*size, c))
+    p.start()
+    proc.append(p)
+
+for p in proc:
+    p.join()
