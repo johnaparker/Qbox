9d30569e48bbcf07400147e1207d811d6ba6bb7a
John Parker <japarker@uchicago.edu>
2015-10-21 02:58:32 -0500

    .h file for hdf5 progress

--- h5out.h
+++ h5out.h
@@ -1,27 +1,36 @@
 #include "H5Cpp.h"
+#include "matrix.h"
 #include <vector>
+#include <map>
+
+//support for matrix class
 
 class node {
 public:
-    H5::DataSpace data;
+    int rank;
+    H5::hsize_t *dims, *maxdims;
+    H5::DataSpace dataspace;  //I don't think this is necessary
     H5::H5std_string name;
+    H5::DataSet dataset;
+
+    bool extendable;
 public:
-    node();
+    node(H5::H5std_string name, gH5::H5File & head, std::vector<H5::hsize_t> size, std::vector<H5::hsize_t> maxsize);
+    void extend(std::vector<H5::hsize_t> size);
+    H5::DataSpace select(std::vector<H5::hsize_t> size, std::vector<H5::hsize_t> offset);
+    void write(double *data, H5::DataSpace fspace);
 };
 
 class h5out {
 public:
-    int rank, Nx, Ny;
-    H5::hsize_t dims[2], maxdims[2];
-     
     H5::H5File outFile;
     H5::H5std_string file_name;
-    std::vector<node> nodes;
-
+    std::map<H5std_string, node> nodes;
 public:
     h5out();
-    void create_node(std::string node_name);
-    void write_to_node();
+    void create_node(H5::H5std_string node_name, estd::vector<H5::hsize_t> size, std::vector<H5::hsize_t> maxsize);
+    void write_to_node(H5::H5std_string, double *data, std::vector<H5::hsize_t> size, std::vector<H5::hsize_t> offset);
+    void write_to_node(H5::H5std_string, matrix & data);
 };
 
 
