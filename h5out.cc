c7e7ed2614e8b78c88ea7c4223e3cce6714a6220
John Parker <japarker@uchicago.edu>
2015-10-22 02:32:45 -0500

    cc file for h5 is largely complete, review needed

--- h5out.cc
+++ h5out.cc
@@ -1,5 +1,6 @@
 #include "h5out.h"
 #include <vector>
+#include "matrix"
 
 
 using namespace std;
@@ -33,21 +34,63 @@ void node::extend(vector<hsize_t> size) {
     if (extendable) {
         hsize_t *dsize = &size[0];
         dataset.extend(dsize);
+        for (int i = 0; i != rank; i++) {
+            dims[i] = dsize[i];
+        }
     }
 }
 
 //actually, add a new variable: the selected dataspace
 //in write, we need both (look into this)
-H5::DataSpace node::select(vector<hsize_t> size, vector<hsize_t> offset) {
-    //DataSpace fspace;               //or return this instead
+void node::select(vector<hsize_t> size, vector<hsize_t> offset) {
     hsize_t *dsize = &size[0];
     hsize_t *doffset = &offset[0];
-    dataspace.selectHyperslab(H5_SELECT_SET, dsize, doffset);   //maybe this is where dataspace is useful
-
-    return dataspace;
+    selected = dataset.getSpace();
+    selected.selectHyperslab(H5_SELECT_SET, dsize, doffset); 
 }
 
 
 void node::write(double *data) {
-    dataset.write(
+    dataset.write(data, PredType::NATIVE_DOUBLE, dataspace, selected);
+}
+
+bool node::isExtendable() {
+    return extendable;
+}
     
+
+h5out::h5out(H5std_string filename): filename(filename) {
+    nodes = {};
+    outFile = H5File(filename, H5F_ACC_TRUNC);
+}
+
+//this shouldn't take maxsize, but rather a bool expandable
+void h5out::create_node(H5std_string node_name, vector<hsize_t> size, vector<hsize_t> maxsize) {
+    node newNode(node_name, size, maxsize);
+    nodes[node_name] = newNode;
+}
+
+//node class should be friended for this
+//this shouldn't take size, but rather a bool expandable
+void write_to_node(H5std_string name, double *data, vector<hsize_t> size) {
+    int rank = nodes[node_name].rank;
+    //check to see if this is the first time writing
+    if (nodes[node_name].isExtendable()) {
+        vector<hsize_t> extend_size(&nodes[node_name].dims[0], &nodes[node_name].dims[rank-1]); 
+        extend_size[rank-1] += 1;
+        nodes[node_name].extend(extend_size);
+    }
+
+    vector<hsize_t> offset(rank, 0);
+    offset[rank-1] = nodes[node_name].dims[rank-1] - 1;
+    nodes[node_name].select(size, iffset);
+
+    nodes[node_name].write(data);
+}
+
+//this shouldn't take size, but rather a bool expandable
+void write_to_node(H5std_string name, matrix & data, vector<hsize_t> size) {
+    write_to_node(name, data.data, size);
+}
+
+
--- h5out.h
+++ h5out.h
@@ -12,6 +12,7 @@ public:
     double fill_value;
     H5::hsize_t *dims, *maxdims;
     H5::DataSpace dataspace;  //I don't think this is necessary
+    H5::DataSpace selected;
     H5::H5std_string name;
     H5::DataSet dataset;
 
@@ -19,8 +20,9 @@ public:
 public:
     node(H5::H5std_string name, gH5::H5File & head, std::vector<H5::hsize_t> size, std::vector<H5::hsize_t> maxsize);
     void extend(std::vector<H5::hsize_t> size);
-    H5::DataSpace select(std::vector<H5::hsize_t> size, std::vector<H5::hsize_t> offset);
-    void write(double *data, H5::DataSpace fspace);
+    void select(std::vector<H5::hsize_t> size, std::vector<H5::hsize_t> offset);
+    void write(double *data);
+    bool isExtendable();
 };
 
 class h5out {
@@ -29,10 +31,10 @@ public:
     H5::H5std_string file_name;
     std::map<H5std_string, node> nodes;
 public:
-    h5out();
-    void create_node(H5::H5std_string node_name, estd::vector<H5::hsize_t> size, std::vector<H5::hsize_t> maxsize);
-    void write_to_node(H5::H5std_string, double *data, std::vector<H5::hsize_t> size, std::vector<H5::hsize_t> offset);
-    void write_to_node(H5::H5std_string, matrix & data);
+    h5out(H5::H5std_string filename);
+    void create_node(H5::H5std_string node_name, std::vector<H5::hsize_t> size, std::vector<H5::hsize_t> maxsize);
+    void write_to_node(H5::H5std_string, double *data, std::vector<H5::hsize_t> size);
+    void write_to_node(H5::H5std_string, matrix & data, std::vector<H5::hsize_t> size);
 };
 
 
