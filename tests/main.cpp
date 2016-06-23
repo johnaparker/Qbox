a465e8d868bc5de0625204016cc8afb21cfe7096
John <japarker@uchicago.edu>
2016-06-23 03:09:36 -0500

    added post/video and deleted old post files

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 using namespace qbox;
 
 int main() {
-    int pml_thickness = 12;
+    int pml_thickness = 20;
     int res = 2;
 
     grid_properties grid(120,120,res,pml_thickness);
-    grid.set_tfsf(12, 12);
+    //grid.set_tfsf(12, 12);
     //grid.set_tfsf({12,12},{68,119});
 
     Field2D test(grid);
@@ -19,13 +19,13 @@ int main() {
     //box_monitor m1("m1",{50,50}, {70,70}, (freq), 100); 
     //surface_monitor m1("m1",{50,50}, {50,70},0,5*f, 100); 
     //surface_monitor m2(m1); 
-    cylinder c1(60,60,5);
-    c1.set_eps(4);
-    test.add_object(c1);
+    //cylinder c1(60,60,5);
+    //c1.set_eps(4);
+    //test.add_object(c1);
     test.add_monitor(m1);
 
-    gaussian_point_source s1({40,60}, 30, 3);
-    //continuous_point_source s1({60,72}, 1/20);
+    //gaussian_point_source s1({60,60}, 30, 3);
+    continuous_point_source s1({60,60}, 1/20.0);
     test.add_source(s1);
 
     for (int i = 0; i != 1000; i++) {
