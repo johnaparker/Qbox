4abd2ef4e2c0e4c809f255c7c05de58af2de74a1
John <japarker@uchicago.edu>
2016-07-14 17:38:51 -0500

    small changes to cmakelists

--- tests/main.cpp
+++ tests/main.cpp
@@ -15,7 +15,7 @@ int main() {
     Field2D test(grid, "out.h5");
     
     double f = 2/30.0;
-    box_monitor m1("m1",{50,50}, {70,70}, 1/30.0,3/30.0, 500, true); 
+    box_monitor m1("m1",{50,50}, {70,70}, 1/30.0,3/30.0, 500, false); 
     //box_monitor m1("m1",{50,50}, {70,70}, (freq), 100); 
     //surface_monitor m1("m1",{50,50}, {50,70},0,5*f, 100); 
     //surface_monitor m2(m1); 
@@ -28,10 +28,10 @@ int main() {
     //continuous_point_source s1({60,60}, 1/20.0);
     test.add_source(s1);
 
-    for (int i = 0; i != 1000; i++) {
+    for (int i = 0; i != 15000; i++) {
         test.update();
-        test.writeE();
-        m1.write();
-        m1.write_sides();
+        //test.writeE();
     }
+    m1.write();
+    m1.write_sides();
 }
