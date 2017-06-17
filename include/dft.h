6fa117d24f216afac7268a0cd1e15ce10cb96004
John <japarker@uchicago.edu>
2017-06-16 22:16:35 -0500

    Moved dft template specialization to cpp file; fixed header guard

--- include/dft.h
+++ include/dft.h
@@ -1,5 +1,5 @@
-#ifndef GUARD_freq_h
-#define GUARD_freq_h
+#ifndef GUARD_dft_h
+#define GUARD_dft_h
 
 #include "h5cpp.h"
 #include "vec.h"
@@ -24,28 +24,6 @@ namespace qbox {
             // h5cpp::write_tensor(imag, group, 'i' + name);
     };
 
-    //template specialization needed because Eigen can't cast when RANK is a template parameter for reasons I know not
-    template<>
-    void complex_dft_tensor<0>::write(const h5cpp::h5group& group, const std::string &name) const {
-        Eigen::Tensor<std::complex<double>, 1, Eigen::RowMajor> combined = 
-                real.cast<std::complex<double>>() + 1i*imag.cast<std::complex<double>>();
-        h5cpp::write_tensor(combined, group, name);
-    }
-
-    template<>
-    void complex_dft_tensor<1>::write(const h5cpp::h5group& group, const std::string &name) const {
-        Eigen::Tensor<std::complex<double>, 2, Eigen::RowMajor> combined = 
-                real.cast<std::complex<double>>() + 1i*imag.cast<std::complex<double>>();
-        h5cpp::write_tensor(combined, group, name);
-    }
-
-    template<>
-    void complex_dft_tensor<2>::write(const h5cpp::h5group& group, const std::string &name) const {
-        Eigen::Tensor<std::complex<double>, 3, Eigen::RowMajor> combined = 
-                real.cast<std::complex<double>>() + 1i*imag.cast<std::complex<double>>();
-        h5cpp::write_tensor(combined, group, name);
-    }
-
     namespace DFT {
         class Ez{};
         class Hx{};
@@ -64,7 +42,7 @@ namespace qbox {
 
         dft(const Array &freq): t(0), freq(freq) {};
 
-        dft(const Array &freq, std::string &name, const std::array<int,RANK> &dim): dft(freq) {
+        dft(const Array &freq, const std::string &name, const std::array<int,RANK> &dim): dft(freq) {
             add(name, dim);
         }
 
@@ -123,53 +101,6 @@ namespace qbox {
 
         std::map<std::string, complex_dft_tensor<RANK>> fourier;
     };
-
-    template<>
-    template<> void dft<0>::update(std::function<std::function<double()>(std::string)> f, double tnew) {
-        if (t == tnew)
-            return;
-        t = tnew;
-        // assert that keys match exactly
-
-        Array cosf = Eigen::cos(2*M_PI*freq*t);
-        Array sinf = Eigen::sin(2*M_PI*freq*t);
-
-        const int kmax = Nfreq();
-        for (auto& p: fourier) {
-            auto g = f(p.first);
-            double g_val = g();
-            for (int k = 0; k < kmax; k++) {
-                p.second.real(k) += g_val*cosf(k);
-                p.second.imag(k) += g_val*sinf(k);
-            }
-        }
-    }
-
-
-    template<>
-    template<> void dft<1>::update(std::function<std::function<double(int)>(std::string)> f, double tnew) {
-        if (t == tnew)
-            return;
-        t = tnew;
-        // assert that keys match exactly
-
-        Array cosf = Eigen::cos(2*M_PI*freq*t);
-        Array sinf = Eigen::sin(2*M_PI*freq*t);
-
-        const int kmax = Nfreq();
-        for (auto& p: fourier) {
-            auto g = f(p.first);
-            const int imax = p.second.real.dimensions()[0];
-
-            for (int i = 0; i < imax; i++ ) {
-                double g_val = g(i);
-                for (int k = 0; k < kmax; k++) {
-                    p.second.real(i,k) += g_val*cosf(k);
-                    p.second.imag(i,k) += g_val*sinf(k);
-                }
-            }
-        }
-    }
 }
 
 #endif
---
+++ src/dft.cpp
@@ -0,0 +1,73 @@
+#include "dft.h"
+
+namespace qbox {
+
+    //template specialization needed because Eigen can't cast when RANK is a template parameter for reasons I know not
+    template<>
+    void complex_dft_tensor<0>::write(const h5cpp::h5group& group, const std::string &name) const {
+        Eigen::Tensor<std::complex<double>, 1, Eigen::RowMajor> combined = 
+                real.cast<std::complex<double>>() + 1i*imag.cast<std::complex<double>>();
+        h5cpp::write_tensor(combined, group, name);
+    }
+
+    template<>
+    void complex_dft_tensor<1>::write(const h5cpp::h5group& group, const std::string &name) const {
+        Eigen::Tensor<std::complex<double>, 2, Eigen::RowMajor> combined = 
+                real.cast<std::complex<double>>() + 1i*imag.cast<std::complex<double>>();
+        h5cpp::write_tensor(combined, group, name);
+    }
+
+    template<>
+    void complex_dft_tensor<2>::write(const h5cpp::h5group& group, const std::string &name) const {
+        Eigen::Tensor<std::complex<double>, 3, Eigen::RowMajor> combined = 
+                real.cast<std::complex<double>>() + 1i*imag.cast<std::complex<double>>();
+        h5cpp::write_tensor(combined, group, name);
+    }
+
+    template<>
+    template<> void dft<0>::update(std::function<std::function<double()>(std::string)> f, double tnew) {
+        if (t == tnew)
+            return;
+        t = tnew;
+        // assert that keys match exactly
+
+        Array cosf = Eigen::cos(2*M_PI*freq*t);
+        Array sinf = Eigen::sin(2*M_PI*freq*t);
+
+        const int kmax = Nfreq();
+        for (auto& p: fourier) {
+            auto g = f(p.first);
+            double g_val = g();
+            for (int k = 0; k < kmax; k++) {
+                p.second.real(k) += g_val*cosf(k);
+                p.second.imag(k) += g_val*sinf(k);
+            }
+        }
+    }
+
+
+    template<>
+    template<> void dft<1>::update(std::function<std::function<double(int)>(std::string)> f, double tnew) {
+        if (t == tnew)
+            return;
+        t = tnew;
+        // assert that keys match exactly
+
+        Array cosf = Eigen::cos(2*M_PI*freq*t);
+        Array sinf = Eigen::sin(2*M_PI*freq*t);
+
+        const int kmax = Nfreq();
+        for (auto& p: fourier) {
+            auto g = f(p.first);
+            const int imax = p.second.real.dimensions()[0];
+
+            for (int i = 0; i < imax; i++ ) {
+                double g_val = g(i);
+                for (int k = 0; k < kmax; k++) {
+                    p.second.real(i,k) += g_val*cosf(k);
+                    p.second.imag(i,k) += g_val*sinf(k);
+                }
+            }
+        }
+    }
+}
