99c0bdfb94704ce3ff09759f63e8e77978d59efb
John <japarker@uchicago.edu>
2017-06-16 22:17:04 -0500

    time_profile can now (optionally) be DFT'd

--- include/sources/time_profile.h
+++ include/sources/time_profile.h
@@ -2,22 +2,30 @@
 #define GUARD_time_profile_h
 
 #include <memory>
+#include <optional>
 #include "h5cpp.h"
+#include "dft.h"
 
 namespace qbox {
 
     class time_profile {
     public:
-        virtual double response(double t) const = 0;
+        time_profile(std::optional<Array> freq = std::nullopt);
+        // This function must be called before response is returned
+        void update(double val, double t);
+
+        virtual double response(double t) = 0;
         virtual void write(const h5cpp::h5group &group) const = 0;
         virtual std::unique_ptr<time_profile> clone() const = 0;
+    private:
+        std::optional<dft<0>> fourier;
     };
 
     class gaussian_time: public time_profile {
     public:
         gaussian_time(double f, double sig, double T0);
 
-        double response(double t) const override;
+        double response(double t) override;
         void write(const h5cpp::h5group &group) const override;
         std::unique_ptr<time_profile> clone() const override;
 
@@ -28,7 +36,7 @@ namespace qbox {
     class continuous_time: public time_profile {
     public:
         continuous_time(double f);
-        double response(double t) const override;
+        double response(double t) override;
         void write(const h5cpp::h5group &group) const override;
 
         std::unique_ptr<time_profile> clone() const override;
--- src/sources/time_profile.cpp
+++ src/sources/time_profile.cpp
@@ -3,10 +3,26 @@
 using namespace std;
 
 namespace qbox {
+
+    time_profile::time_profile(optional<Array> freq) {
+        if (freq)
+            fourier = dft<0>(*freq, "dft", {});
+    }
+
+    void time_profile::update(double val, double t) {
+        if (fourier) {
+            function<double()> f = [val](){return val;};
+            function<function<double()>(string)> g = [f](string){return f;};
+            fourier->update<double()>(g, t);
+        }
+    }
+
     gaussian_time::gaussian_time(double f, double sig, double T0): f(f), sig(sig), T0(T0) {};
 
-    double gaussian_time::response(double t) const {
-        return sin(2*M_PI*f*t)*exp(-1*(pow((t-T0)*sig*2*M_PI,2)));
+    double gaussian_time::response(double t) {
+        double val = sin(2*M_PI*f*t)*exp(-1*(pow((t-T0)*sig*2*M_PI,2)));
+        update(val, t);
+        return val;
     }
 
     void gaussian_time::write(const h5cpp::h5group &group) const {
@@ -21,8 +37,10 @@ namespace qbox {
 
     continuous_time::continuous_time(double f): f(f) {};
 
-    double continuous_time::response(double t) const {
-        return sin(2*M_PI*f*t);
+    double continuous_time::response(double t) {
+        double val = sin(2*M_PI*f*t);
+        update(val, t);
+        return val;
     }
 
     void continuous_time::write(const h5cpp::h5group &group) const {
